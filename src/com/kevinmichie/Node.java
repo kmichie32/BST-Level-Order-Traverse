72021c43e25fcedc42f6f31835a36f4c64702fee
Kevin Michie <kmichie32@gmail.com>
2016-04-26 20:04:52 -0500

    Level Order Tree Traversal
    http://www.geeksforgeeks.org/level-order-tree-traversal/
    
    Linked Lists are the bane of my existence at this point, but I will
    persist until I get it!
    
    This is unfinished, but I will hopefully complete this tomorrow

---
+++ src/com/kevinmichie/Node.java
@@ -0,0 +1,10 @@
+package com.kevinmichie;
+
+public class Node {
+        Node left,right;
+        int data;
+        Node(int data){
+            this.data=data;
+            left=right=null;
+        }
+    }
---
+++ src/com/kevinmichie/Solution.java
@@ -0,0 +1,70 @@
+package com.kevinmichie;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static int getNumberOfNodes(Node root) {
+
+        if (root == null) { // if the tree is empty, return 0
+            return 0;
+        } else {
+            return  1+ Math.max(getNumberOfNodes(root.left), getNumberOfNodes(root.right));
+        }
+    }
+
+    public static int getHeight(Node root){
+        return getNumberOfNodes(root) - 1;
+        // returns the height which is 1 less than the max length of connected nodes
+    }
+
+    public static void printGivenLevel(Node root, int level){
+        if (root == null){
+            return;
+        }
+        if (level == 1){
+            System.out.println(root.data + "");
+        }
+        else if(level > 1){
+            printGivenLevel(root.left, level-1);
+            printGivenLevel(root.right, level-1);
+        }
+    }
+
+
+    public static void levelOrder(Node root){
+        int levelsOfBST = getHeight(root);
+        for(int i = 0; i <= levelsOfBST; i++){
+            printGivenLevel(root, i);
+        }
+    }
+
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        levelOrder(root);
+    }
+}
+
